8506978ab207fd8f2f81a668042e1e8e0e5289b7
ohmez <kkid_92@hotmail.com>
2018-09-21 17:13:49 -0600

    js updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,9 @@
-// first step is to populate the page
 var started = false; 
 var timer;
+var time = 30; 
+var intervalId
+// end global variables 
+
 $(document).ready(function() {
 $(".y").hide();
 console.log($(".container"));
@@ -13,6 +16,7 @@ $(document).on("click", "#startBtn", function() {
     started = true; 
     console.log(started);
     questionBtns();
+    run();
 });
 // end start on click function 
 function questionBtns () {
@@ -28,3 +32,24 @@ function questionBtns () {
     console.log("this is when append happens");
     questionA.html(Btns);
 };
+// end trivia button populate function 
+function run() {
+    intervalId = setInterval(decrease, 1000);
+    console.log(intervalId);
+};
+// end run function for timer 
+function decrease () {
+    time--; 
+    $("span#timer").html(time);
+    if (time <= 0) {
+        stop();
+    }
+};
+// end interval decrease and populate function for timer 
+function stop () { 
+    intervalId = clearInterval(intervalId);
+    console.log(intervalId);
+    time = 30; 
+    $("span#timer").html(time);
+};
+// end stop timer function also resets timer and populates
