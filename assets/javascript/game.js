669c5b900888a6c7f57dcc55aba93136487e68eb
ohmez <kkid_92@hotmail.com>
2018-09-21 18:04:10 -0600

    js questions and answers populate

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,13 @@ var started = false;
 var timer;
 var time = 30; 
 var intervalId
+var trivia = {
+    q1: {question: 'What village is Naruto from?', 
+        a:['The Village', 'The Village People', 'Village Hidden in The Leaf', 'Village Hidden in The Fog']
+        }
+    };
 // end global variables 
+console.log(trivia.q1);
 
 $(document).ready(function() {
 $(".y").hide();
@@ -15,24 +21,29 @@ $(document).on("click", "#startBtn", function() {
     $(".y").show();
     started = true; 
     console.log(started);
-    questionBtns();
+    answerBtns();
     run();
+    questionGen();
 });
 // end start on click function 
-function questionBtns () {
+function answerBtns () {
     var Btns = [];
-    var questionA = $("#questionArea")
+    var answerA = $("#answers");
     var newBtn = $("<button>");
     for (i = 0; i < 4; i++) {
-        Btns[i] = $("<button>").text('test'+i);
+        Btns[i] = $("<button>").text(trivia.q1.a[i]);
         // newBtn.text('test'+i);
         // Btns.push(newBtn);
         console.log(Btns);
     }
     console.log("this is when append happens");
-    questionA.html(Btns);
+    answerA.html(Btns);
 };
 // end trivia button populate function 
+function questionGen () {
+    var questionA = $("#questionArea");
+    questionA.html('<p>'+ trivia.q1.question + '</p>');
+};
 function run() {
     intervalId = setInterval(decrease, 1000);
     console.log(intervalId);
